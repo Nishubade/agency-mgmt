548bb83ea8b9efcda61258902d2da7adefa84bdd
Raghav <arg.ahv7@gmail.com>
2022-11-25 20:33:15 +0545

    c

--- src/sections/projects/view/index.js
+++ src/sections/projects/view/index.js
@@ -3,3 +3,4 @@ export { default as BasicInfoCard } from './BasicInfoCard';
 export { default as ChartCard } from './ChartCard';
 export { default as MoreInfoCard } from './MoreInfoCard';
 export { default as ViewTabs } from './ViewTabs';
+export { default as ProjectViewComp } from './ProjectView';
