f6a8a2e2b9ede4cf17726523aa3445deabbce4fa
Raghav <arg.ahv7@gmail.com>
2022-12-05 15:43:35 +0545

    action

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material';
+import { Chip, Container } from '@mui/material';
 import DashboardLayout from '@layouts/dashboard';
 import { Page } from '@components/page';
 import { useSettingsContext } from '@components/settings';
@@ -6,6 +6,7 @@ import { DashboardComponent } from '@sections/dashboard';
 import { ContextProvider } from '@sections/dashboard/context';
 import AuthGuard from '@guards/AuthGuard';
 import { DashboardProvider } from '@contexts/dashboard';
+import { useRahatTrigger } from '@services/contracts/useRahatTrigger';
 
 // ----------------------------------------------------------------------
 
@@ -15,12 +16,20 @@ Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default function Dashboard() {
   const { themeStretch } = useSettingsContext();
+  const { isLive } = useRahatTrigger();
+
+  const breadCrumbAction = (
+    <Chip
+      label={isLive ? 'Multi-Sig Trigger Live' : 'Multi-Sig Trigger Not Live'}
+      color={isLive ? 'success' : 'error'}
+    />
+  );
 
   return (
     <AuthGuard>
       <DashboardProvider>
         <ContextProvider>
-          <Page title={PAGE_TITLE} nocard>
+          <Page title={PAGE_TITLE} nocard breadcrumbAction={breadCrumbAction}>
             <Container maxWidth={themeStretch ? false : 'xl'}>
               <DashboardComponent />
             </Container>
