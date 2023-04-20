38aff587ee51c1ba9af96e573eb28da9fb393aed
Santosh Shrestha <santosh@rumsan.com>
2023-04-20 17:56:56 +0545

    misc

--- src/sections/dashboard/Dashboard.js
+++ src/sections/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Box, Button, Card, CardActions, CardContent, CardHeader, Grid, Stack } from '@mui/material';
 import SummaryCard from '@components/SummaryCard';
